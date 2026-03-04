10808254a9154f92782b135890d65a6a903de069
VictorSomers <v.somers@students.ephec.be>
2026-03-04 17:22:23 +0100

    fin 5.1 tp3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,43 @@
 import { useEffect, useState } from "react";
+import "./App.css";
+
+
+
 // Définition d'une interface pour le typage
 // Sera couvert plus en profondeur en TH
 interface User {
-id: number;
-name: string;
+  id: number;
+  nom: string;
+  prenom: string;
+  createdAt: string;
+  updatedAt: string;
 }
 function App() {
-// 1. Définition de l'état
-const [data, setData] = useState<User[]>([]);
-// 2. Appel API au montage du composant
-useEffect(() => {
-fetch("http://localhost:3000/api/users")
-.then(res => res.json())
-.then(result => setData(result))
-.catch(err => console.error(err));
-}, []);
-// 3. Rendu (JSX)
-return (
-<div>
-<h1>Liste des utilisateurs</h1>
-<ul>
-{data.map((item) => (
-<li key={item.id}>{item.name}</li>
-))}
-</ul>
-</div>
-);
+  // 1. Définition de l'état
+  const [users, setUsers] = useState<User[]>([]);
+  // 2. Appel API au montage du composant
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+    .then(res => res.json())
+    .then(result => setUsers(result))
+    .catch(err => console.error(err));
+  }, []);
+
+  // 3. Rendu (JSX)
+  return (
+    <div>
+      <h1>Liste des utilisateurs</h1>
+
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.prenom} {user.nom}
+          </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
+
 export default App;
 
